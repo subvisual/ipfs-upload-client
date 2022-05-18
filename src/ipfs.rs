eb552eb3a58cb7802de0981f01053fe347d6b1d3
Ze Pedro Resende <17102689+ZePedroResende@users.noreply.github.com>
2022-05-18 01:37:21 +0100

    Add cli interface and ipfs file uploader
    Why ?
    * We want a compatible interface with the (infura ipfs client)[https://github.com/INFURA/ipfs-upload-client]
    * We need a http lib to be able to upload files to ipfs
    
    How ?
    * Add clap interface
    * Simple cli interface
    * Add http lib for ipfs

---
+++ src/ipfs.rs
@@ -0,0 +1,62 @@
+use reqwest::{multipart, multipart::Part, Body, Client};
+use tokio::fs::File;
+use tokio_util::codec::{BytesCodec, FramedRead};
+
+pub struct IPFS {
+    client: Client,
+    url: String,
+    id: String,
+    secret: String,
+}
+
+impl IPFS {
+    pub fn new(url: String, id: String, secret: String) -> Self {
+        IPFS {
+            client: Client::new(),
+            url,
+            id,
+            secret,
+        }
+    }
+
+    pub async fn add_file(&self, file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
+        let url = format!("{url}/api/v0/add", url = self.url);
+        let token = format!("{}:{}", self.id, self.secret);
+
+        let file = File::open(file_path).await?;
+
+        let form = multipart::Form::new().part("file", file_to_body(file));
+
+        let response = self
+            .client
+            .post(url)
+            .bearer_auth(token)
+            .multipart(form)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        Ok(response)
+    }
+
+    //pub async fn add_dir(
+    //    &self,
+    //    file_paths : Vec<&str>,
+    //) -> Result<String, Box<dyn std::error::Error>> {
+    //    let url = format!(
+    //        "{url}/api/v0/add?wrap-with-directory=true",
+    //        url = self.url
+    //    );
+
+    //    let response = self.client.post(url).multipart().send()?.await?;
+    //    Ok(response)
+    //}
+}
+
+fn file_to_body(file: File) -> Part {
+    let stream = FramedRead::new(file, BytesCodec::new());
+    let body = Body::wrap_stream(stream);
+
+    Part::stream(body)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+mod ipfs;
+
+use crate::ipfs::IPFS;
+use clap::Parser;
+use std::fs::metadata;
+
+const INFURA_API: &str = "https://ipfs.infura.io:5001";
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    id: String,
+    #[clap(short, long)]
+    secret: String,
+    #[clap(short, long, default_value_t = String::from(INFURA_API))]
+    url: String,
+    #[clap(short, long, default_missing_value = "true")]
+    pin: String,
+    #[clap(help = "The file path or directory to upload")]
+    path: String,
+}
+
+impl Args {
+    fn pin_value(m: String) -> bool {
+        m.parse().expect("non boolean value for pin")
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let id = args.id;
+    let secret = args.secret;
+    let url = args.url;
+    let pin_value = Args::pin_value(args.pin);
+    let path = args.path;
+
+    let is_file: bool = metadata(path.clone()).expect("not a valid path").is_file();
+
+    if is_file {
+        let api = IPFS::new(url, id, secret);
+        let out = api.add_file(&path).await;
+        println!("{:?}", out);
+    }
 }
