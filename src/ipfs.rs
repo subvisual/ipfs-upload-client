d229ed81cd07c8ad3fe858a9973816bdc68cef41
ZePedroResende <j.p.resende68@gmail.com>
2022-05-18 19:39:02 +0100

    Add directory upload

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -1,4 +1,10 @@
-use reqwest::{multipart, multipart::Part, Body, Client};
+use futures::{stream, StreamExt};
+use reqwest::{
+    multipart,
+    multipart::{Form, Part},
+    Body, Client,
+};
+use std::sync::{Arc, Condvar, RwLock};
 use tokio::fs::File;
 use tokio_util::codec::{BytesCodec, FramedRead};
 
@@ -40,18 +46,63 @@ impl IPFS {
         Ok(response)
     }
 
-    //pub async fn add_dir(
-    //    &self,
-    //    file_paths : Vec<&str>,
-    //) -> Result<String, Box<dyn std::error::Error>> {
-    //    let url = format!(
-    //        "{url}/api/v0/add?wrap-with-directory=true",
-    //        url = self.url
-    //    );
+    pub async fn add_directory(
+        &self,
+        file_paths: Vec<String>,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let mut form = multipart::Form::new();
 
-    //    let response = self.client.post(url).multipart().send()?.await?;
-    //    Ok(response)
-    //}
+        //for file_path in file_paths {
+        //    let file = File::open(file_path).await?;
+        //    form.part("file", file_to_body(file));
+        //}
+        //
+        let bodies = stream::iter(file_paths)
+            .map(|path| self.process_path(path.to_owned()))
+            .buffer_unordered(100)
+            .collect::<Vec<Result<Part, Box<dyn std::error::Error>>>>()
+            .await;
+
+        for b in bodies {
+            match b {
+                Ok(body) => {
+                    form = form.part("file", body);
+                }
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+
+        loop {
+            let url = format!("{url}/api/v0/add?wrap-with-directory=true", url = self.url);
+            let token = format!("{}:{}", self.id, self.secret);
+            let response = self
+                .client
+                .post(url)
+                .bearer_auth(token)
+                .multipart(form)
+                .send()
+                .await?
+                .text()
+                .await?;
+
+            return Ok(response);
+        }
+    }
+
+    //       let new_form: Arc<RwLock<Form>> = bodies.fold(form, move |form_move, body| {
+    //           if let Ok(form_locked) = form.write() {
+    //               form_locked.part("file", body);
+    //           }
+
+    //           form
+    //       });
+    async fn process_path(&self, path: String) -> Result<Part, Box<dyn std::error::Error>> {
+        let future_file = File::open(path);
+        let file: File = future_file.await?;
+
+        let body = file_to_body(file);
+        Ok(body)
+    }
 }
 
 fn file_to_body(file: File) -> Part {
