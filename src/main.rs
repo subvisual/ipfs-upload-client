dae1525147cb80955d0cb1f72e3ba53097bbf94d
Resende <17102689+ZePedroResende@users.noreply.github.com>
2022-07-18 19:43:33 +0100

    Add cli interface and ipfs file uploader (#1)
    * Add cli interface and ipfs file uploader
    
    Why ?
    * We want a compatible interface with the (infura ipfs client)[https://github.com/INFURA/ipfs-upload-client]
    * We need a http lib to be able to upload files to ipfs
    
    How ?
    * Add clap interface
    * Simple cli interface
    * Add http lib for ipfs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+mod ipfs;
+
+use crate::ipfs::IPFS;
+use clap::Parser;
+use std::fs::metadata;
+
+const INFURA_API: &str = "https://ipfs.infura.io:5001";
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    id: String,
+    #[clap(short, long)]
+    secret: String,
+    #[clap(short, long, default_value_t = String::from(INFURA_API))]
+    url: String,
+    #[clap(short, long, default_missing_value = "true")]
+    pin: String,
+    #[clap(help = "The file path or directory to upload")]
+    path: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let id = args.id;
+    let secret = args.secret;
+    let url = args.url;
+    let path = args.path;
+
+    let is_file: bool = metadata(path.clone()).expect("not a valid path").is_file();
+
+    if is_file {
+        let api = IPFS::new(url, id, secret);
+        let out = api.add_file(&path).await;
+        println!("{:?}", out);
+    }
 }
